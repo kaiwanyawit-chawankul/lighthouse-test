5e18cdf3c6743153a55c4452d62cd8e083661e53
Kaiwanyawit Chawankul <kaiwanyawit.c@thoughtworks.com>
2021-11-22 21:05:23 +0700

    make CI fail

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,6 +4,12 @@ module.exports = {
         /* Add configuration here */
         url: ['https://www.thoughtworks.com/']
       },
+      assert: {
+        assertions: {
+          'categories:performance': ['warn', {minScore: 0.3}],
+          'categories:accessibility': ['error', {minScore: 0.9}]
+        }
+      },
       upload: {
         /* Add configuration here */
         target: 'lhci',
